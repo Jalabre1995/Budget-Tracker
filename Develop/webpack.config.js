4091bf89e99bbe4d71a9f1a87dad14c56e0666b9
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-04-03 12:15:33 -0400

    Update Webpack.confog.js

--- Develop/webpack.config.js
+++ Develop/webpack.config.js
@@ -1,12 +1,44 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest")
+const path = require( "path");
 const config = {
     entry: {
-        app: "./src/app/js"
+        app: "./src/index/js"
     },
     output: {
         path:__dirname + "/public/dist",
-        filename: "bundle.js"
+        filename: "[name].bundle.js"
     },
-    mode: "development",
+    mode: "production",
+    plugins: [
+        new WebpackPwaManifest({
+            filename: "Budget app",
+            short_name: "Budget",
+            description: "An application to track your transaction history from your budget.",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            start_url: "/",
+            display: "standalone",
+            icons: [{
+                src: path.resolve(__dirname, "public/icons/icon-192x192.png"),
+                size: [192,515],
+            }]
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset=env"]
+                    }
+                }
+            }
+        ]
+    }
+    
 };
 
 module.exports = config;
